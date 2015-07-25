2e77805f99c714ded5e10d2acdfe9dd9b17dd8a7
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2015-07-25 10:26:06 +0300

    initial commit

---
+++ iPhogAmination/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhogAmination
+//
+//  Created by Nikolay Shubenkov on 25/07/15.
+//  Copyright (c) 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
